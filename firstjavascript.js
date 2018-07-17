ec8b38bd7074b07ad61ee43d86137e0deff9eba5
mgkaliski <40776416+mgkaliski@users.noreply.github.com>
2018-07-17 15:12:50 -0700

    Add files via upload

---
+++ firstjavascript.js
@@ -0,0 +1,8 @@
+//changing the heading of the HTML
+function changeHeading(){
+  document.getElementById("heading").innerHTML = "Nice to meet you";
+}
+
+function changeColor(){
+  document.getElementById("button").style.display = "none";
+}
